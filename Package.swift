99427bb4ae85adf78bb0a7b84fb537218a9c4184
extrawurst <mail@rusticorn.com>
2024-04-26 10:22:58 +0200

    Initial Commit

---
+++ Package.swift
@@ -0,0 +1,23 @@
+// swift-tools-version: 5.10
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "bevy_ios_iap",
+    products: [
+        // Products define the executables and libraries a package produces, making them visible to other packages.
+        .library(
+            name: "bevy_ios_iap",
+            targets: ["bevy_ios_iap"]),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
+        .target(
+            name: "bevy_ios_iap"),
+        .testTarget(
+            name: "bevy_ios_iapTests",
+            dependencies: ["bevy_ios_iap"]),
+    ]
+)
