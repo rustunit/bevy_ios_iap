43fc81cb10d3fe97cdcd8654d5a4b06b2e81797b
extrawurst <mail@rusticorn.com>
2024-05-05 22:17:54 +0200

    new binary release

--- Package.swift
+++ Package.swift
@@ -17,8 +17,8 @@ let package = Package(
         // Targets can depend on other targets in this package and products from dependencies.
         .binaryTarget(
             name: "RustXcframework",
-            url: "https://github.com/rustunit/bevy_ios_iap/releases/download/rs-0.1.0/RustXcframework.xcframework.zip",
-            checksum: "0772432798d2800aba31e5bc52a8ad248598efcfbf5d78a2dba11b0bee7c7f60"),
+            url: "https://github.com/rustunit/bevy_ios_iap/releases/download/rs-0.1.1/RustXcframework.xcframework.zip",
+            checksum: "02ce1e897058362cf4c12db55f557c6708dd8b8dc8e4bd34fd7e895e4b3fbf26"),
         .target(
             name: "bevy_ios_iap",
             dependencies: ["RustXcframework"]),
